ec40565f4d41fc32b763dbd82dc0e1390664c72c
denkata1232 <104383416+denkata1232@users.noreply.github.com>
2025-06-02 14:00:29 +0300

    Update Form1.cs

--- cursorFlashLight/Form1.cs
+++ cursorFlashLight/Form1.cs
@@ -9,7 +9,7 @@
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            string message = "То е като мозък с жици и мишка,\r\nЗнае всичко, ала не е книжка.\r\nПише с код, а не с химикал.\r\nПиташ ли го – всичко си разбрал.\r\nЖивее в офиса, вкъщи дори.\r\nКакво е това, ще ми кажеш нали?\r\n";
+            string message = "В междучасието там се бърза,\r\nгладни ученици там се изръсват.\r\nНе е столова, но там храна се намира.\r\nКъде отиваш, като стомахът вибрира?\r\nСандвич, кроасан или бонбон,\r\nвсичко има в училищния ____________ .\r\n";
             int radius = txbRadius.Text != "" ? int.Parse(txbRadius.Text) : 50;
             FormPlayState playState = new FormPlayState(message, radius);
             playState.Show();
